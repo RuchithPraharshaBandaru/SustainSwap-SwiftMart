180e551623e4e1f1383bd7b086be25c4c60ea010
RuchithPraharshaBandaru <ruchithpraharshabandaru@gmail.com>
2025-05-15 02:26:43 +0530

    change

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ app.get("*", (req, res) => {
 
 
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   dbConnection();
   console.log(`Server running on http://localhost:${PORT}`)
